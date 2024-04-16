c5c9b2dccb0d0060174a74fddae333ec39934904
Riquelme-Carlos <carlosriquelmedeveloper@gmail.com>
2024-04-15 20:30:34 -0600

    Adding project to repo

---
+++ 2857176_PersonalityQuiz/2857176_PersonalityQuiz/PersonalityQuiz/PersonalityQuiz/IntroductionViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//  AL02857176
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+
+    @IBAction func unwindToQuizIntroduction(segue: UIStoryboardSegue) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
